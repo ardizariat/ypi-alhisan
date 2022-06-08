b05d8e15820e32967995f21acc47f1c349f65f62
ardizariat <ardizariat@gmail.com>
2022-06-08 21:23:01 +0700

    faker

--- database/seeders/PengurusYayasanSeeder.php
+++ database/seeders/PengurusYayasanSeeder.php
@@ -2,21 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class PengurusYayasanSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
+
         $faker = Faker::create('id_ID');
 
         $users = [
@@ -91,3 +85,4 @@ class PengurusYayasanSeeder extends Seeder
             ]);
         }
     }
+}
