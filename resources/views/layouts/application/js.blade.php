24a8c494e5ba430ba53ed995ccb1cae3cd3fcbbe
ardizariat <ardizariat@gmail.com>
2022-06-11 15:48:10 +0700

    dev

--- resources/views/layouts/application/js.blade.php
+++ resources/views/layouts/application/js.blade.php
@@ -1,11 +1,11 @@
  <!-- Vendor JS Files -->
- <script src="{{ asset('application/vendor/aos/aos.js') }}"></script>
+ {{-- <script src="{{ asset('application/vendor/aos/aos.js') }}"></script> --}}
  <script src="{{ asset('application/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
- <script src="{{ asset('application/vendor/glightbox/js/glightbox.min.js') }}"></script>
+ {{-- <script src="{{ asset('application/vendor/glightbox/js/glightbox.min.js') }}"></script>
  <script src="{{ asset('application/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
  <script src="{{ asset('application/vendor/swiper/swiper-bundle.min.js') }}"></script>
  <script src="{{ asset('application/vendor/waypoints/noframework.waypoints.js') }}"></script>
- <script src="{{ asset('application/vendor/php-email-form/validate.js') }}"></script>
+ <script src="{{ asset('application/vendor/php-email-form/validate.js') }}"></script> --}}
 
  <!-- Template Main JS File -->
  <script src="{{ asset('application/js/main.js') }}"></script>
